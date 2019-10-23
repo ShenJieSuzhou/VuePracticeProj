fa9c113e8917afad58884776c34811f35bd939e4
ShenJieSuzhou <745689122@qq.com>
2019-10-23 17:59:07 +0800

    测试工程代码提交

---
+++ app.vue
@@ -0,0 +1,21 @@
+<template>
+    <div> hello {{ name }}</div>
+</template>
+
+<script>
+export default {
+    data () {
+        return {
+            name: 'vue.js'
+        }
+    }
+}
+</script>
+
+<style>
+    div{
+        color: aqua;
+        font-size: 24px;
+    }
+</style>
+>
---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+var path = require('path');
+// var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var VueLoaderPlugin = require('vue-loader/lib/plugin'); 
+var htmlWebpackPlugin = require('html-webpack-plugin');
+
+var config = {
+    entry: {
+        main: './main.js'
+    },
+    output: {
+        path: path.join(__dirname, './dist'),
+        publicPath: '/',
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.vue$/,
+                use: 'vue-loader'
+            },
+            {
+                test: /\js$/,
+                use: 'babel-loader',
+                exclude: /node_modules/
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ],
+            }
+        ]
+    },
+    resolve: {
+        alias: {
+            "vue$": "vue/dist/vue.js"
+        }
+    },
+    plugins: [
+        // make sure to include the plugin for the magic
+        new VueLoaderPlugin(),
+        new htmlWebpackPlugin({
+            template: path.join(__dirname, './index.html'),
+            filename: 'index.html'
+        })
+    ],
+    
+};
+
+module.exports = config;
